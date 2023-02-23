86d95492dad02ce8afa2e4b595a1f978746a6e3c
gaushao <jimiaspampa@gmail.com>
2023-02-23 12:17:57 -0300

    footer version

--- src/app/Page.tsx
+++ src/app/Page.tsx
@@ -4,6 +4,11 @@
  */
 
 import ASSETS from "./assets";
+import pkg from "../../package.json";
+
+const DOCS_URL = "https://gaushao.github.io/d3-react-globe/docs";
+const GIT_URL = "https://github.com/Gaushao/d3-react-globe";
+const VERSION = pkg.version;
 
 function logo(src: string, url: string) {
   const style = {
@@ -20,15 +25,23 @@ function logo(src: string, url: string) {
   };
 }
 
-const DOCS_URL = "https://gaushao.github.io/d3-react-globe/docs";
-const GIT_URL = "https://github.com/Gaushao/d3-react-globe";
-
-const page: React.CSSProperties = {
+const logos: React.CSSProperties = {
   position: "fixed",
   margin: 16,
   zIndex: 1,
-  backgroundColor: "white",
   borderRadius: 5,
+  backgroundColor: "white",
+};
+
+const footer: React.CSSProperties = {
+  display: pkg.version ? "flex" : "none",
+  position: "fixed",
+  padding: 4,
+  bottom: 16,
+  right: 16,
+  zIndex: 1,
+  borderRadius: 5,
+  backgroundColor: "white",
 };
 
 /**
@@ -42,10 +55,11 @@ const page: React.CSSProperties = {
 export default function Page({ children }: React.PropsWithChildren<{}>) {
   return (
     <>
-      <div style={page}>
+      <div style={logos}>
         <img {...logo(ASSETS.git, GIT_URL)} />
         <img {...logo(ASSETS.book, DOCS_URL)} />
       </div>
+      <div style={footer}>v{pkg.version}</div>
       {children}
     </>
   );
